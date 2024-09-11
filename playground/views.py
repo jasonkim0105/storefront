6fe4169a9f0df4fca46138719a264ad2c5283981
Jason <k.jason0105@gmail.com>
2024-09-11 13:52:05 -0500

    playground views

--- playground/views.py
+++ playground/views.py
@@ -4,8 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 #request -> response
 #request handler, it is a called an action in node.js
+
+def calculate():
+  x = 1
+  y = 1
+  return x
+
 def say_hello(request):
   #pull datea from db
   #transform
   #send email
+  x = calculate()
   return render(request, 'hello.html', { 'name': 'Jason' })
